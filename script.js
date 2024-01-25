efe8c2ef232feb8993edbb8f77785708774dad87
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-01-25 10:38:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,35 +6,42 @@ class Person {
     this._age = age;
   }
 
+  // Getter for name
   get name() {
     return this._name;
   }
 
+  // Setter for age
   set age(value) {
     this._age = value;
   }
 }
 
 class Student extends Person {
+  // Constructor for Student class
   constructor(name, age) {
     super(name, age);
   }
 
+  // Method specific to Student class
   study() {
     console.log(${this.name} is studying);
   }
 }
 
 class Teacher extends Person {
+  // Constructor for Teacher class
   constructor(name, age) {
     super(name, age);
   }
 
+  // Method specific to Teacher class
   teach() {
     console.log(${this.name} is teaching);
   }
 }
 
+// Assigning classes to the window object
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
