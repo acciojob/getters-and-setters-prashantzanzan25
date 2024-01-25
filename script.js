32ca1cef09a670f0926c7c45f9077949db07b838
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-01-25 10:45:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,41 @@
-// Class definitions
-
 class Person {
-  constructor(name, age) {
-    this._name = name;
-    this._age = age;
-  }
+    constructor(name, age) {
+        this.Name = name;
+        this.Age = age;
+    }
 
-  // Getter for name
-  get name() {
-    return this._name;
-  }
+    // get getName() {
+    //     console.log(The name is ${this.Name});
+    // }
+	get getName(){ 
+		return this.name
+	}
 
-  // Setter for age
-  set age(value) {
-    this._age = value;
-  }
+    set setage(age) {
+     //   console.log(The age is ${newage});
+		this.Age = age;
+    }
+	
 }
 
 class Student extends Person {
-  // Constructor for Student class
-  constructor(name, age) {
-    super(name, age);
-  }
+    constructor(name, age) {
+        super(name, age);
+    }
 
-  // Method specific to Student class
-  study() {
-    console.log(${this.name} is studying);
-  }
+    study() {
+        console.log(${this.Name} is studying);
+    }
 }
 
 class Teacher extends Person {
-  // Constructor for Teacher class
-  constructor(name, age) {
-    super(name, age);
-  }
-
-  // Method specific to Teacher class
-  teach() {
-    console.log(${this.name} is teaching);
-  }
+    teach() {
+        console.log(${this.Name} is teaching);
+    }
 }
 
-// Assigning classes to the window object
+// Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
